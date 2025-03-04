fc97f1ca1b13ea479d7e18781c908fae25a3f981
jordan <jordanpieroborda@gmail.com>
2025-03-04 10:14:10 -0500

    Mensaje descriptivo

--- docs/conf.py
+++ docs/conf.py
@@ -1,53 +1,56 @@
 # Configuration file for the Sphinx documentation builder.
-#
-# For the full list of configuration options, see the documentation:
-# https://www.sphinx-doc.org/en/master/usage/configuration.html
-
-# -- Project information -----------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
 import os
 import sys
-sys.path.insert(0, os.path.abspath('..'))  # Añade la ruta de tu proyecto (si es necesario)
+
+#sys.path.insert(0, os.path.abspath('..'))
+
+# -- Project information -----------------------------------------------------
 
 project = 'Manual para Read the Docs'
 copyright = '2023, Jordan Borda'
 author = 'Jordan Borda'
 release = '0.1'
+version = '0.1'
 
 # -- General configuration ---------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
 extensions = [
-    'sphinx.ext.autodoc',
-    'sphinx.ext.napoleon', #google docstring para python
-    'sphinx.ext.todo',
-    'sphinx.ext.viewcode',
-    'sphinx.ext.intersphinx'
+    "myst_parser",
+    "sphinx_copybutton",
+    "sphinx_design",
+    "sphinx_tabs.tabs",
+    "sphinx-prompt",
+    "sphinx.ext.autodoc",
+    "sphinx.ext.autosectionlabel",
+    "sphinx.ext.extlinks",
+    "sphinx.ext.intersphinx",
 ]
 
-templates_path = ['_templates']
+templates_path = ['_templates']  # Si tienes plantillas personalizadas
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
 language = 'es'
 
 # -- Options for HTML output -------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
 html_theme = 'sphinx_rtd_theme'
 html_static_path = ['_static']
-html_logo = '_static/mi_logo.png'  # Agrega la ruta a tu logo
-html_favicon = '_static/favicon.ico' # Agrega la ruta al favicon (opcional)
+
+# Ruta al logo (asegúrate de que sea correcta)
+html_logo = '_static/mi_logo.png'  # <---- ¡VERIFICAR ESTA RUTA!
+html_favicon = '_static/favicon.ico' # Agregar Favicon
 
 html_theme_options = {
-    'logo_only': False,  # Cambia a True si solo quieres el logo, sin el nombre del proyecto
-    'display_version': False, # Ocultar la versión de la documentación
-    'logo': '_static/mi_logo.png' # Opcional: Especifica de nuevo la ruta al logo dentro de las opciones del tema
+    "logo_only": False, # Cambiar a True si solo quieres el logo
+    "display_version": False, # Ocultar la versión
+    "logo": '_static/mi_logo.png' #Especificar logo
 }
 
-# -- Extension configuration -------------------------------------------------
-
-# -- Options for todo extension ----------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/extensions/todo.html#configuration
-
-todo_include_todos = True
+html_context = {
+    "conf_py_path": "/docs/",  # Ajusta si es necesario
+    "display_github": True,
+    "github_user": "jordanborda", # Tu nombre de usuario de GitHub
+    "github_repo": "SGD_V5",    # Nombre de tu repositorio
+    "github_version": "main",  # Rama
+}
